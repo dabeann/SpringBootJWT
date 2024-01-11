8049888ea2e0f05d3babb0778eea0f77dd8c8b79
dabeann <dabin4078@daum.net>
2024-01-11 16:55:53 +0900

    feat: AuthorityRepository

---
+++ jwtTutorial/src/main/java/jwtjwt/jwtTutorial/repository/AuthorityRepository.java
@@ -0,0 +1,7 @@
+package jwtjwt.jwtTutorial.repository;
+
+import jwtjwt.jwtTutorial.entity.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
