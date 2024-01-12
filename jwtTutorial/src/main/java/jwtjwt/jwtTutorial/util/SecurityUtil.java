2a6865ed2e1a1714c7f603aa49f9a928774cf025
dabeann <dabin4078@daum.net>
2024-01-12 14:30:59 +0900

    comment: SecurityUtil

--- jwtTutorial/src/main/java/jwtjwt/jwtTutorial/util/SecurityUtil.java
+++ jwtTutorial/src/main/java/jwtjwt/jwtTutorial/util/SecurityUtil.java
@@ -9,11 +9,22 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Optional;
 
 public class SecurityUtil {
+    /**
+     * SecurityUtil class: 간단한 유틸리티 메서드 구현 클래스
+     */
 
     private static final Logger logger = LoggerFactory.getLogger(SecurityUtil.class);
 
     private SecurityUtil() {}
 
+    /**
+     * getCurrentUsername(): SecurityContext의 Authentication 객체를 이용해 username을 반환하는 유틸성 메서드
+     * 1. SecurityContextHolder에서 Authentication 객체를 꺼냄
+     * 2. authentication - getPrincipal() - getUsername() 순서대로 유저명을 꺼내 반환
+     *
+     * SecurityContext에 Authentication 객체가 저장되는 시점은 JwtFilter의 doFilter() 메서드에서
+     * Request가 들어올 때 Authentication 객체를 저장해서 사용한다.
+     */
     public static Optional<String> getCurrentUsername() {
         final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
