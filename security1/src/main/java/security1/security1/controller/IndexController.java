c277e271bb90003e3c9567e513c5d6081c00062a
dabeann <dabin4078@daum.net>
2024-01-15 16:41:27 +0900

    feat: Spring Boot Security 1 - 환경설정

---
+++ security1/src/main/java/security1/security1/controller/IndexController.java
@@ -0,0 +1,17 @@
+package security1.security1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IndexController {
+
+    @GetMapping({ "", "/" })
+    public String index() {
+        // 머스테치 기본폴터 src/main/resources/
+        // 뷰리졸버 설정: templates (prefix), .mustache (suffix) 생략가능
+        return "index"; // src/main/resources/templates/index.mustache
+    }
+
+}
